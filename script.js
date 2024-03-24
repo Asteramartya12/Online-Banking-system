99fc4fd49fe8cf7b5ffab844f740ddef2cce12bc
Amartya Roy <asteramartya12@gmail.com>
2024-03-24 22:43:35 +0530

    commit from amartya

--- script.js
+++ script.js
@@ -21,14 +21,17 @@ window.addEventListener("click", function(event) {
   }
 });
 
-$(".up").click(function(){scroll(0,0)});
+document.addEventListener('DOMContentLoaded', function () {
+  window.addEventListener('scroll', function () {
+      var animatedSection = document.getElementById('animated-section');
+      var position = animatedSection.getBoundingClientRect().top;
+      var windowHeight = window.innerHeight;
 
-const bars = document.querySelectorAll('.bar');
-
-bars.forEach((bar, index) => {
-  setTimeout(() => {
-    bar.style.height = `${bar.dataset.height}%`;
-  }, index * 200);
+      if (position < windowHeight * 0.9) {
+          animatedSection.classList.add('show');
+      }
+  });
 });
+
 
 
